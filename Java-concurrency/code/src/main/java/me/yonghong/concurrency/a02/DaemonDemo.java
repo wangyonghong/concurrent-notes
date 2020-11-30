5b643e1b6739ba081ebec5fa5f4446630653fff2
wangyonghong <yonghongwang@163.com>
2020-11-30 22:50:17 +0800

    :tada: update

---
+++ Java-concurrency/code/src/main/java/me/yonghong/concurrency/a02/DaemonDemo.java
@@ -0,0 +1,26 @@
+package me.yonghong.concurrency.a02;
+
+public class DaemonDemo {
+    public static void main(String[] args) {
+        Thread daemonThread = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println("i am alive");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("finally block");
+                }
+            }
+        });
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        //确保main线程结束前能给daemonThread能够分到时间片
+        try {
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
